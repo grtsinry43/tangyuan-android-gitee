bd7d5cfe5b364e8361321ad445db392dd047600e
线粒体XianlitiCN <mitochondriacn@gmail.com>
2025-03-05 13:11:53 +0800

    修改User结构匹配后端

--- app/src/main/java/com/qingshuige/tangyuan/network/User.java
+++ app/src/main/java/com/qingshuige/tangyuan/network/User.java
@@ -1,10 +1,11 @@
 package com.qingshuige.tangyuan.network;
 
-public class User
-{
+public class User {
     public int userId;
     public String nickName;
     public String phoneNumber;
     public String isoRegionName;
     public String email;
+    public String bio;
+    public String avatarGuid;
 }
