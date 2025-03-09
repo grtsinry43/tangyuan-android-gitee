238f767f629227c689a866cb0005376c1f2633d1
线粒体XianlitiCN <mitochondriacn@gmail.com>
2025-03-10 00:44:11 +0800

    完成发帖功能，但暂不开放发图片帖功能

--- app/src/main/java/com/qingshuige/tangyuan/NewPostActivity.java
+++ app/src/main/java/com/qingshuige/tangyuan/NewPostActivity.java
@@ -1,26 +1,49 @@
 package com.qingshuige.tangyuan;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.content.res.AppCompatResources;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.qingshuige.tangyuan.network.CreatPostMetadataDto;
+import com.qingshuige.tangyuan.network.PostBody;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class NewPostActivity extends AppCompatActivity {
 
@@ -30,6 +53,9 @@ public class NewPostActivity extends AppCompatActivity {
     private ImageView imageView1;
     private ImageView imageView2;
     private ImageView imageView3;
+
+    private ExecutorService es;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +79,9 @@ public class NewPostActivity extends AppCompatActivity {
         imageView1 = (ImageView) findViewById(R.id.imageView1);
         imageView2 = (ImageView) findViewById(R.id.imageView2);
         imageView3 = (ImageView) findViewById(R.id.imageView3);
+
+        es = Executors.newSingleThreadExecutor();
+        handler = new Handler(Looper.getMainLooper());
 
         textEdit.addTextChangedListener(new TextWatcher() {
             @Override
@@ -91,17 +120,107 @@ public class NewPostActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //选择图片
         if (item.getItemId() == R.id.image_button) {
-            //假如imageView有空闲
-            if (imageView1.getDrawable() == null ||
-                    imageView2.getDrawable() == null ||
-                    imageView3.getDrawable() == null) {
-                //选择图片
-                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intent, 1);
-            }
+            //TODO:修复后端静态图片Bug，再开放发图片帖功能
+            Toast.makeText(this, "后端目前有图片帖Bug，因此暂不开放发图片帖功能。", Toast.LENGTH_LONG)
+                    .show();
+            return super.onOptionsItemSelected(item);
+//            //假如imageView有空闲
+//            if (imageView1.getDrawable() == null ||
+//                    imageView2.getDrawable() == null ||
+//                    imageView3.getDrawable() == null) {
+//                //选择图片
+//                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//                startActivityForResult(intent, 1);
+//            }
         }
+
+        //发帖
+        if (item.getItemId() == R.id.send_post_button) {
+            sendPostAsync(this);
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private void sendPostAsync(Context context) {
+        es.execute(new Runnable() {
+            @Override
+            public void run() {
+                //1.上传图片
+                List<String> guids = new ArrayList<>();
+                List<ImageView> imageViewList = new ArrayList<>();
+                imageViewList.add(imageView1);
+                imageViewList.add(imageView2);
+                imageViewList.add(imageView3);//这个List只是为了foreach方便
+                for (ImageView v : imageViewList) {
+                    if (v.getDrawable() != null) {
+                        Bitmap bitmap = ((BitmapDrawable) v.getDrawable()).getBitmap();
+                        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                        byte[] bytes = stream.toByteArray();
+                        RequestBody requestBody = RequestBody.create(MediaType.parse("image/jpeg"), bytes);
+                        MultipartBody.Part part =
+                                MultipartBody.Part.createFormData("file", "image.jpg", requestBody);
+                        try {
+                            String guid =
+                                    new ArrayList<>(TangyuanApplication.getApi().postImage(part).execute().body().values())
+                                            .get(0);
+                            guids.add(guid);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+
+                //2.上传元数据
+                int postId;
+                CreatPostMetadataDto metadataDto = new CreatPostMetadataDto();
+                metadataDto.userId = 1;//TODO:用户系统
+                metadataDto.postDateTime = new Date();
+                metadataDto.sectionId = 1;
+                metadataDto.isVisible = true;
+                try {
+                    postId = new ArrayList<>(TangyuanApplication.getApi().postPostMetadata(metadataDto).execute().body().values())
+                            .get(0);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                //3.上传Body
+                PostBody body = new PostBody();
+                body.postId = postId;
+                body.textContent = textEdit.getText().toString();
+                if (!guids.isEmpty()) {
+                    body.image1UUID = guids.get(0);
+                    if (guids.get(1) != null) {
+                        body.image1UUID = guids.get(1);
+                    }
+                    if (guids.get(2) != null) {
+                        body.image1UUID = guids.get(2);
+                    }
+                }
+                Log.i("TY", "PostBody is: " +
+                        new GsonBuilder().serializeNulls().create().toJson(body));
+                try {
+                    TangyuanApplication.getApi().postPostBody(body).execute();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                //4.完成
+                finish();
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(context, R.string.post_sent, Toast.LENGTH_SHORT).show();
+                        context.startActivity(new Intent(context, PostActivity.class).putExtra("postId", postId));
+                    }
+                });
+            }
+        });
+
     }
 
     @Override
@@ -114,7 +233,7 @@ public class NewPostActivity extends AppCompatActivity {
                     if (imageView1.getDrawable() == null) {
                         imageView1.setImageURI(data.getData());
                         break;
-                    } else if (imageView2.getDrawable() == null){
+                    } else if (imageView2.getDrawable() == null) {
                         imageView2.setImageURI(data.getData());
                         break;
                     } else if (imageView3.getDrawable() == null) {
--- app/src/main/java/com/qingshuige/tangyuan/network/ApiInterface.java
+++ app/src/main/java/com/qingshuige/tangyuan/network/ApiInterface.java
@@ -5,9 +5,14 @@ import com.qingshuige.tangyuan.viewmodels.PostInfo;
 import java.util.List;
 import java.util.Map;
 
+import okhttp3.MultipartBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
 
 //用于Retrofit
@@ -17,18 +22,22 @@ public interface ApiInterface {
     Call<PostMetadata> getPostMetadata(@Path("id") int id);
 
     @GET("post/body/{id}")
-    Call<PostBody> getPostBody(@Path("id")int id);
+    Call<PostBody> getPostBody(@Path("id") int id);
 
     @GET("user/{id}")
-    Call<User> getUser(@Path("id")int id);
+    Call<User> getUser(@Path("id") int id);
 
     @GET("post/metadata/random/{count}")
-    Call<List<PostMetadata>> getRandomPostMetadata(@Path("count")int count);
+    Call<List<PostMetadata>> getRandomPostMetadata(@Path("count") int count);
 
     @POST("post/metadata")
-    Call<Map<String,Integer>> postPostMetadata(CreatPostMetadataDto metadata);
+    Call<Map<String, Integer>> postPostMetadata(@Body CreatPostMetadataDto metadata);
 
     @POST("post/body")
-    Call<String> postPostBody(PostBody body);
+    Call<ResponseBody> postPostBody(@Body PostBody body);
+
+    @Multipart
+    @POST("image/uploadjpg")
+    Call<Map<String, String>> postImage(@Part MultipartBody.Part file);
 
 }
