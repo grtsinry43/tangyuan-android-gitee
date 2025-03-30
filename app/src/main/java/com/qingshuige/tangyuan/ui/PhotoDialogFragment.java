8422a39aab0ef307ab73a0e459742c785c5a8363
线粒体XianlitiCN <mitochondriacn@gmail.com>
2025-03-30 15:42:58 +0800

    完成看图对话框

--- app/src/main/java/com/qingshuige/tangyuan/ui/PhotoDialogFragment.java
+++ app/src/main/java/com/qingshuige/tangyuan/ui/PhotoDialogFragment.java
@@ -8,28 +8,44 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+import com.github.chrisbanes.photoview.PhotoView;
 import com.qingshuige.tangyuan.R;
 
 public class PhotoDialogFragment extends DialogFragment {
-    @NonNull
+
     @Override
-    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        LayoutInflater inflater = requireActivity().getLayoutInflater();
-        View root = inflater.inflate(R.layout.photo_dialog_fragment, null);
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setView(root);
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.photo_dialog_fragment, container, false);
         Drawable drawable = new BitmapDrawable((Bitmap) getArguments().getParcelable("bitmap"));
+        ((PhotoView) root.findViewById(R.id.photoView)).setImageDrawable(drawable);
 
-        ((ImageView) root.findViewById(R.id.photoView)).setImageDrawable(drawable);
+        return root;
+    }
 
-        return builder.create();
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (getDialog() != null && getDialog().getWindow() != null) {
+            getDialog().getWindow().setBackgroundDrawableResource(R.color.black);
+            getDialog().getWindow().setFlags(
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            getDialog().getWindow().setLayout(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT);
+            getDialog().getWindow().setStatusBarColor(android.graphics.Color.TRANSPARENT);
+            getDialog().getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+        }
     }
 
     public static PhotoDialogFragment create(Bitmap bitmap) {
