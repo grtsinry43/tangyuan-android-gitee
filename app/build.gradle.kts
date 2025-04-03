bcd7c11fbe885f27a0bd20c91eedcfd3b062bc54
线粒体XianlitiCN <mitochondriacn@gmail.com>
2025-04-03 14:52:53 +0800

    更新版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.qingshuige.tangyuan"
         minSdk = 28
         targetSdk = 35
-        versionCode = 20
-        versionName = "0.2.0"
+        versionCode = 100
+        versionName = "1.0.0 风"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
